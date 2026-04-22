568ea406b7d9d658052d9e8c2f53a9111745b093
Tran Duy Khai <88431307+TranKhai19@users.noreply.github.com>
2026-04-22 22:46:13 +0700

    Delete read_excel.py

--- read_excel.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-with open('output.txt', 'w', encoding='utf-8') as f:
-    f.write('--- BOOK1.XLSX ---\n')
-    f.write(pd.read_excel('e:/FinalProject/DoAnTotNghiep/Book1.xlsx').to_markdown())
-    f.write('\n--- SCENARIO.XLSX ---\n')
-    f.write(pd.read_excel('e:/FinalProject/DoAnTotNghiep/Scenario.xlsx').to_markdown())
